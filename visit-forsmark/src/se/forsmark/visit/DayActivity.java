1b294e1a7c2583ca2d9fa75e1fce6ecb203a0821
Joel Lindholm <curry@gamerarmy.com>
2012-10-15 11:38:53 +0000

    git-svn-id: http://192.168.1.79/svn/visit-forsmark@4 22f7ca1f-e406-4609-ac91-1203f0195763

---
+++ visit-forsmark/src/se/forsmark/visit/DayActivity.java
@@ -0,0 +1,13 @@
+package se.forsmark.visit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DayActivity extends Activity{
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main); // CHANGE
+		
+	}
+}
---
+++ visit-forsmark/src/se/forsmark/visit/TermsActivity.java
@@ -0,0 +1,12 @@
+package se.forsmark.visit;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TermsActivity extends Activity{
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main); // CHANGE
+	}
+}
