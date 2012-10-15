c260e7bc8e44cb82897b8643f94c53aac0953f7c
Joel Lindholm <curry@gamerarmy.com>
2012-10-15 12:36:07 +0000

    git-svn-id: http://192.168.1.79/svn/visit-forsmark@8 22f7ca1f-e406-4609-ac91-1203f0195763

--- visit-forsmark/src/se/forsmark/visit/ContactActivity.java
+++ visit-forsmark/src/se/forsmark/visit/ContactActivity.java
@@ -7,6 +7,6 @@ public class ContactActivity extends Activity{
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.contactview); // CHANGE
+		setContentView(R.layout.contactview);
 	}
 }
