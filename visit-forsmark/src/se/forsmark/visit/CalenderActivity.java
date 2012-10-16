81eaca5e1bed1f3f58429ebf9e1394116e70fa6e
Joel Lindholm <curry@gamerarmy.com>
2012-10-16 15:43:10 +0000

    git-svn-id: http://192.168.1.79/svn/visit-forsmark@47 22f7ca1f-e406-4609-ac91-1203f0195763

--- visit-forsmark/src/se/forsmark/visit/CalenderActivity.java
+++ visit-forsmark/src/se/forsmark/visit/CalenderActivity.java
@@ -1,6 +1,8 @@
 package se.forsmark.visit;
 
 import java.util.Calendar;
+import java.util.Date;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,22 +13,32 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class CalenderActivity extends Activity {
+
+	private int curMonth;
+	private TextView tvMonth;
+	private TextView tvYear;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		getWindow().requestFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.calenderview);
-		
+
 		Calendar c = Calendar.getInstance();
+		curMonth = c.get(Calendar.MONTH);
 		c.set(Calendar.WEEK_OF_MONTH, 1);
 		c.set(Calendar.DAY_OF_WEEK, 2);
-		setCalender(c); // Initialize calendar
-		
+
+		tvMonth = (TextView) findViewById(R.id.border_title);
+		tvYear = (TextView) findViewById(R.id.border_small);
 		Button bb = (Button) findViewById(R.id.button_back_top);
-		Button bn  = (Button) findViewById(R.id.button_next_top);
+		Button bn = (Button) findViewById(R.id.button_next_top);
+		tvYear.setVisibility(View.VISIBLE);
 		bn.setVisibility(View.VISIBLE);
 		bb.setVisibility(View.VISIBLE);
+
+		setCalender(c); // Initialize calendar
 	}
 
 	public void setCalender(Calendar c) {
@@ -35,14 +47,22 @@ public class CalenderActivity extends Activity {
 		LinearLayout row;
 		Button dateCell;
 		TextView restCell;
-
-		String[] s = this.getResources().getStringArray(R.array.calStringsSwe);
 		
+		Date date = new Date();
+
+		String[] days = this.getResources().getStringArray(
+				R.array.calDaysStringsSwe);
+		String[] month = this.getResources().getStringArray(
+				R.array.calMonthStringsSwe);
+
+		tvMonth.setText(month[date.getMonth()]);
+		tvYear.setText(String.valueOf(1900 + date.getYear()));
+
 		row = (LinearLayout) findViewById(R.id.weekDays);
 
 		for (int k = 0; k < 8; k++) {
 			restCell = (TextView) row.getChildAt(k);
-			restCell.setText(s[k]);
+			restCell.setText(days[k]);
 		}
 
 		for (int i = 0; i < 6; i++) {
@@ -51,6 +71,9 @@ public class CalenderActivity extends Activity {
 			for (int k = 0; k < 8; k++) {
 				if (k > 0) {
 					dateCell = (Button) row.getChildAt(k);
+					if (c.get(Calendar.MONTH) != curMonth) {
+						dateCell.setBackgroundResource(R.drawable.cell_button_not_now);
+					}
 					dateCell.setText(String.valueOf(c.get(Calendar.DATE)));
 					c.add(Calendar.DATE, +1);
 				} else {
@@ -60,10 +83,25 @@ public class CalenderActivity extends Activity {
 				}
 			}
 		}
+	}
+
+	public void onDateClick(View v) {
 
 	}
 	
-	public void onDateClick(View v){
-		
+	public void topPreButton(View v){
+		Calendar c = Calendar.getInstance();
+		c.add(Calendar.MONTH, -1);
+		c.set(Calendar.WEEK_OF_MONTH, 1);
+		c.set(Calendar.DAY_OF_WEEK, 2);
+		setCalender(c);
+	}
+	
+	public void topPostButton(View v){
+		Calendar c = Calendar.getInstance();
+		c.add(Calendar.MONTH, +1);
+		c.set(Calendar.WEEK_OF_MONTH, 1);
+		c.set(Calendar.DAY_OF_WEEK, 2);
+		setCalender(c);
 	}
 }
