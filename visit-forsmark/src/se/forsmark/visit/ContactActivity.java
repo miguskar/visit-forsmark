23e9651ea624642c74e122546f707b6d639b82e9
Mikael Karlsson <miguskar@gmail.com>
2012-10-16 09:39:58 +0000

    Adding first template for activities
    git-svn-id: http://192.168.1.79/svn/visit-forsmark@15 22f7ca1f-e406-4609-ac91-1203f0195763

--- visit-forsmark/src/se/forsmark/visit/ContactActivity.java
+++ visit-forsmark/src/se/forsmark/visit/ContactActivity.java
@@ -2,8 +2,10 @@ package se.forsmark.visit;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
 public class ContactActivity extends Activity{
 	@Override
@@ -11,6 +13,9 @@ public class ContactActivity extends Activity{
 		super.onCreate(savedInstanceState);
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.contactview);
+		setContentView(R.layout.template);
+		View v = View.inflate(this, R.layout.contactview, null);
+		RelativeLayout l = (RelativeLayout) findViewById(R.id.activity_container);
+		l.addView(v);
 	}
 }
