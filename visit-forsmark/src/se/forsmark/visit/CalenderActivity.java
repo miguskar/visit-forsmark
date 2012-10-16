67f7478d8a57aadb5b522df41b628e94606e2df1
Johan Thorsberg <sjt_91@hotmail.com>
2012-10-16 09:41:51 +0000

    git-svn-id: http://192.168.1.79/svn/visit-forsmark@16 22f7ca1f-e406-4609-ac91-1203f0195763

--- visit-forsmark/src/se/forsmark/visit/CalenderActivity.java
+++ visit-forsmark/src/se/forsmark/visit/CalenderActivity.java
@@ -1,16 +1,47 @@
 package se.forsmark.visit;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
-public class CalenderActivity extends Activity{
+public class CalenderActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		getWindow().requestFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.calenderview);
+
+		TableLayout cal = (TableLayout) findViewById(R.id.calendarTable);
+		TableRow row;
+		TextView cell;
+
+		Calendar c = Calendar.getInstance();
+		c.set(Calendar.WEEK_OF_MONTH, 1);
+		c.set(Calendar.DAY_OF_WEEK, 2);
+
+		String append;
+
+		for (int i = 1; i < 7; i++) {
+			row = (TableRow) cal.getChildAt(i);
+
+			for (int k = 0; k < 8; k++) {
+				cell = (TextView) row.getChildAt(k);
+
+				if (k > 0) {
+					cell.setText(String.valueOf(c.get(Calendar.DATE)));
+					c.add(Calendar.DATE, +1);
+				} else {
+					cell.setText(String.valueOf(c.get(Calendar.WEEK_OF_YEAR)));
+				}
+			}
+		}
 	}
 }
