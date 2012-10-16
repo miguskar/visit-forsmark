5291284c4cfd4429e0025aae258bbc94a89cd898
Joel Lindholm <curry@gamerarmy.com>
2012-10-16 12:38:36 +0000

    git-svn-id: http://192.168.1.79/svn/visit-forsmark@32 22f7ca1f-e406-4609-ac91-1203f0195763

--- visit-forsmark/src/se/forsmark/visit/CalenderActivity.java
+++ visit-forsmark/src/se/forsmark/visit/CalenderActivity.java
@@ -1,16 +1,13 @@
 package se.forsmark.visit;
 
 import java.util.Calendar;
-import java.util.Date;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
-import android.widget.TableLayout;
-import android.widget.TableRow;
 import android.widget.TextView;
 
 public class CalenderActivity extends Activity {
@@ -19,43 +16,41 @@ public class CalenderActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.template);
+		setContentView(R.layout.calenderview);
+		
+		//View v = View.inflate(this, R.layout.calenderview, null);
+		//RelativeLayout l = (RelativeLayout) findViewById(R.id.activity_container);
+		//l.addView(v);
 
-		View v = View.inflate(this, R.layout.calenderview, null);
-		RelativeLayout l = (RelativeLayout) findViewById(R.id.activity_container);
-		l.addView(v);
-
-		TableLayout cal = (TableLayout) v.findViewById(R.id.calendarTable);
-		TableRow row;
+		LinearLayout cal = (LinearLayout) findViewById(R.id.calendarTable);
+		LinearLayout row;
 		TextView cell;
 
 		Calendar c = Calendar.getInstance();
 		c.set(Calendar.WEEK_OF_MONTH, 1);
 		c.set(Calendar.DAY_OF_WEEK, 2);
-
+		
 		String[] s = this.getResources().getStringArray(R.array.calStringsSwe);
+		row = (LinearLayout) cal.getChildAt(0);
+		
+		for (int k = 0; k < 8; k++) {
+			cell = (TextView) row.getChildAt(k);
+			cell.setText(s[k]);
+		}
+		
+		for (int i = 1; i < 7; i++) {
+			row = (LinearLayout) cal.getChildAt(i);
 
-		for (int i = 0; i < 7; i++) {
-			row = (TableRow) cal.getChildAt(i);
-			if (i > 0) {
-				for (int k = 0; k < 8; k++) {
-					cell = (TextView) row.getChildAt(k);
+			for (int k = 0; k < 8; k++) {
+				cell = (TextView) row.getChildAt(k);
 
-					if (k > 0) {
-						cell.setText(String.valueOf(c.get(Calendar.DATE)));
-						c.add(Calendar.DATE, +1);
-					} else {
-						cell.setText(String.valueOf(c
-								.get(Calendar.WEEK_OF_YEAR)));
-					}
-				}
-			}else{
-				for (int k = 0; k < 8; k++) {
-					cell = (TextView) row.getChildAt(k);
-					cell.setText(s[k]);
+				if (k > 0) {
+					cell.setText(String.valueOf(c.get(Calendar.DATE)));
+					c.add(Calendar.DATE, +1);
+				} else {
+					cell.setText(String.valueOf(c.get(Calendar.WEEK_OF_YEAR)));
 				}
 			}
-
 		}
 	}
 }
