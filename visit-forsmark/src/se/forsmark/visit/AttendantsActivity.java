7397a26dfb69eda5004dfd36eb1511d9f7823764
Mikael Karlsson <miguskar@gmail.com>
2012-10-16 15:41:56 +0000

    Renaming activity
    git-svn-id: http://192.168.1.79/svn/visit-forsmark@46 22f7ca1f-e406-4609-ac91-1203f0195763

--- visit-forsmark/src/se/forsmark/visit/AttendantsActivity.java
+++ visit-forsmark/src/se/forsmark/visit/AttendantsActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class AttendentsActivity extends Activity{
+public class AttendantsActivity extends Activity{
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
--- visit-forsmark/src/se/forsmark/visit/ContactActivity.java
+++ visit-forsmark/src/se/forsmark/visit/ContactActivity.java
@@ -1,23 +1,66 @@
 package se.forsmark.visit;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class ContactActivity extends Activity{
+public class ContactActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.contactview); //Use template xml
-		
-		initialize(); //Initialize views
+		setContentView(R.layout.contactview);
+
+		initialize(); // Initialize views
 	}
-	
+
+	/**
+	 * Unhides elements etc..
+	 */
 	private void initialize() {
+		//Set Title
+		TextView tv = (TextView) findViewById(R.id.border_title);
+		tv.setText(R.string.ContactActivityTitle);
+		//Display progress
+		ImageView iv = (ImageView) findViewById(R.id.border_progress);
+		iv.setVisibility(View.VISIBLE); //Unhide progress
+		iv.setBackgroundResource(R.drawable.border_step_one); //Display step one
+		//Display hint
+		Button b = (Button) findViewById(R.id.button_hint_top);
+		b.setVisibility(View.VISIBLE);
+		b.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				String text = getResources().getString(R.string.ContactActivityToast);
+				Toast t = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+				t.setGravity(Gravity.TOP|Gravity.RIGHT, 0, 0);
+				t.show();
+				
+			}
+		});
 		
+	}
+
+	public void bottomBackClick(View v) {
 		
+	}
+
+	public void bottomCancelClick(View v) {
+
+	}
+
+	public void bottomNextClick(View v) {
+		Intent i = new Intent(getApplicationContext(), AttendantsActivity.class);
+		startActivity(i);
 	}
 }
