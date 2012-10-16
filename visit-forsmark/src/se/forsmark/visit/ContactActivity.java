3e88058b345901925980e06aa31e5d3f34c77b3b
Mikael Karlsson <miguskar@gmail.com>
2012-10-16 11:31:11 +0000

    git-svn-id: http://192.168.1.79/svn/visit-forsmark@23 22f7ca1f-e406-4609-ac91-1203f0195763

--- visit-forsmark/src/se/forsmark/visit/ContactActivity.java
+++ visit-forsmark/src/se/forsmark/visit/ContactActivity.java
@@ -13,7 +13,12 @@ public class ContactActivity extends Activity{
 		super.onCreate(savedInstanceState);
 		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.template);
+		setContentView(R.layout.template); //Use template xml
+		
+		initialize(); //Initialize views
+	}
+	
+	private void initialize() {
 		View v = View.inflate(this, R.layout.contactview, null);
 		RelativeLayout l = (RelativeLayout) findViewById(R.id.activity_container);
 		l.addView(v);
