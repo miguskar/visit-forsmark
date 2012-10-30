8d7516b0d6430c84445ee62ad929dfb2daa9cabe
Joel Lindholm <curry@gamerarmy.com>
2012-10-30 14:37:37 +0000

    git-svn-id: http://192.168.1.79/svn/visit-forsmark@64 22f7ca1f-e406-4609-ac91-1203f0195763

---
+++ visit-forsmark/src/se/forsmark/visit/DayListAdapter.java
@@ -0,0 +1,56 @@
+
+import java.util.ArrayList;
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class DayListAdapter extends ArrayAdapter<DayListItem> {
+	private ArrayList<DayListItem> entries;
+	private Activity activity;;
+
+	public DayListAdapter(Activity a, int textViewResourceId,
+			ArrayList<DayListItem> entries) {
+		super(a, textViewResourceId, entries);
+		this.entries = entries;
+		this.activity = a;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View v = convertView;
+		ViewHolder holder;
+
+		DayListItem day = entries.get(position);
+
+		if (v == null) {
+			LayoutInflater inflater = (LayoutInflater) activity
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			v = inflater.inflate(R.layout.daylistitem, null);
+			holder = new ViewHolder();
+			holder.item1 = (TextView) v.findViewById(R.id.itemStart);
+			holder.item2 = (TextView) v.findViewById(R.id.itemSeats);
+			holder.item3 = (TextView) v.findViewById(R.id.itemEnd);
+			v.setTag(holder);
+		} else {
+			holder = (ViewHolder) v.getTag();
+		}
+
+		if (day != null) {
+			holder.item1.setText(day.getStart());
+			holder.item2.setText(day.getSeats());
+			holder.item3.setText(day.getEnd());
+		}
+		return v;
+	}
+
+	public static class ViewHolder {
+		public TextView item1;
+		public TextView item2;
+		public TextView item3;
+	}
+}
