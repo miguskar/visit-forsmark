80a4b610b1beadecd2a0385d95d71458e6884ac6
Johan Thorsberg <sjt_91@hotmail.com>
2012-10-30 14:42:24 +0000

    git-svn-id: http://192.168.1.79/svn/visit-forsmark@66 22f7ca1f-e406-4609-ac91-1203f0195763

--- visit-forsmark/src/se/forsmark/visit/DayActivity.java
+++ visit-forsmark/src/se/forsmark/visit/DayActivity.java
@@ -1,7 +1,23 @@
 package se.forsmark.visit;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -12,7 +28,9 @@ public class DayActivity extends Activity{
 	
 	private TextView tvDate;
 	private TextView tvYear;
-	
+	private int curMonth;
+	private int curYear;
+	private int curDate;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -20,25 +38,31 @@ public class DayActivity extends Activity{
 		getWindow().requestFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.dayview); // CHANGE
 		Bundle extras = getIntent().getExtras();
+		
 		Initialize(extras);
 	}
 	
 	public void Initialize(Bundle extras) {
-		String day = extras.getString("DATE");
-		int month = extras.getInt("MONTH");
-		int year = extras.getInt("YEAR");
+		curDate = extras.getInt("DATE");
+		curMonth = extras.getInt("MONTH");
+		curYear = extras.getInt("YEAR");
+		
 		String[] mon = this.getResources().getStringArray(R.array.calMonthStringsSwe);
 		
 		tvDate = (TextView) findViewById(R.id.border_title);
 		tvYear = (TextView) findViewById(R.id.border_small);
-		tvDate.setText(day + " " + mon[month]);
+		tvDate.setText(curDate + " " + mon[curMonth]);
 		tvYear.setVisibility(View.VISIBLE);
-		tvYear.setText(String.valueOf(year));
+		tvYear.setText(String.valueOf(curYear));
 		
 		Button bb = (Button) findViewById(R.id.button_back_top);
 		Button bn = (Button) findViewById(R.id.button_next_top);
 		bn.setVisibility(View.VISIBLE);
 		bb.setVisibility(View.VISIBLE);
+		
+		Log.v("jennie suger", ""+curMonth);
+		Log.v("jsäm", curYear+"");
+		Log.v("asd", "asdas");
 	}
 	
 	public void topBackButton(View v) {
@@ -47,5 +71,51 @@ public class DayActivity extends Activity{
 
 	public void topNextButton(View v) {
 		
+	}
+	
+	public String[] getDateInfo() {
+		String result = "";
+		InputStream is = null;
+		// http post
+		try {
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpPost httppost = new HttpPost(this.getString(R.string.httpRequestUrl));
+
+			List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+			pairs.add(new BasicNameValuePair("case", "getDayInfo"));
+			pairs.add(new BasicNameValuePair("date", "" + (curDate + 1)));
+			pairs.add(new BasicNameValuePair("month", "" + (curMonth + 1)));
+			pairs.add(new BasicNameValuePair("year", "" + curYear));
+			httppost.setEntity(new UrlEncodedFormEntity(pairs));
+
+			HttpResponse response = httpclient.execute(httppost);
+			HttpEntity entity = response.getEntity();
+			is = entity.getContent();
+		} catch (Exception e) {
+			Log.e("log_tag", "Error in http connection " + e.toString());
+		}
+		// convert response to string
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					is, "iso-8859-1"), 8);
+			StringBuilder sb = new StringBuilder();
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				sb.append(line + "\n");
+			}
+			is.close();
+
+			result = sb.toString();
+
+		} catch (Exception e) {
+			Log.e("log_tag", "Error converting result " + e.toString());
+		}
+		Log.v("r", "r: " + result);
+		result = result.substring(1, result.length() - 2);
+		result = result.replace("\"", "");
+
+		String[] tmp = result.split(",");
+
+		return tmp;
 	}
 }
--- visit-forsmark/src/se/forsmark/visit/DayListAdapter.java
+++ visit-forsmark/src/se/forsmark/visit/DayListAdapter.java
@@ -1,3 +1,4 @@
+package se.forsmark.visit;
 
 import java.util.ArrayList;
 import android.app.Activity;
@@ -6,7 +7,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class DayListAdapter extends ArrayAdapter<DayListItem> {
