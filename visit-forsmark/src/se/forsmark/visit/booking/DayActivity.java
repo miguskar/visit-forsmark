e62da1707aafd66382e0781c24ee99dbd08c9775
Mikael Karlsson <miguskar@gmail.com>
2012-11-28 13:44:53 +0000

    git-svn-id: http://192.168.1.79/svn/visit-forsmark@187 22f7ca1f-e406-4609-ac91-1203f0195763

--- visit-forsmark/src/se/forsmark/visit/booking/DayActivity.java
+++ visit-forsmark/src/se/forsmark/visit/booking/DayActivity.java
@@ -22,6 +22,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -41,6 +42,8 @@ public class DayActivity extends Activity {
 	private int curDate;
 	private ArrayList<DayListItem> array;
 	private DayListAdapter adapter;
+	OnItemClickListener itemListener;
+	private EventPrinter ep;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -48,7 +51,33 @@ public class DayActivity extends Activity {
 		getWindow().requestFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.dayview);
 		Bundle extras = getIntent().getExtras();
+		itemListener = new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View v, int pos, long id) {
+				DayListItem day = array.get(pos);
+				Date todayDate = new Date();
+				int startHour = Integer.valueOf(day.getStart().substring(0, 2));
+				int startMin = Integer.valueOf(day.getStart().substring(3, 5));
+				Date theDate = new Date(curYear - 1900, curMonth, curDate, startHour, startMin);
+				Log.v("ThedateDate", theDate.toString());
+				Log.v("TodayDate", todayDate.toString());
 
+				if (theDate.compareTo(todayDate) > 0) { 
+					if (!day.getSeats().equals(getString(R.string.noSeats))) {
+						Intent dialog = new Intent(getApplicationContext(), AttendantsDialogActivity.class);
+						dialog.putExtra("ID", day.getId());
+						dialog.putExtra("DATE", curDate);
+						dialog.putExtra("MONTH", curMonth);
+						dialog.putExtra("YEAR", curYear);
+						dialog.putExtra("START", day.getStart());
+						dialog.putExtra("SEATS", Integer.parseInt(day.getSeats()));
+						dialog.putExtra("END", day.getEnd());
+						startActivity(dialog);
+					}
+				} else {
+					Toast.makeText(getApplicationContext(), R.string.backInTime, Toast.LENGTH_LONG).show();
+				}
+			}
+		};
 		Initialize(extras, savedInstanceState);
 	}
 
@@ -63,7 +92,8 @@ public class DayActivity extends Activity {
 	@Override
 	protected void onResume() {
 		super.onResume();
-		printEvents(0);
+		ep = new EventPrinter();
+		ep.execute(0);
 	}
 
 	public void Initialize(Bundle extras, Bundle savedInstanceState) {
@@ -94,96 +124,7 @@ public class DayActivity extends Activity {
 		list.setAdapter(adapter);
 
 		list.setOnItemClickListener(itemListener);
-		printEvents(0);
 
-	}
-
-	OnItemClickListener itemListener = new OnItemClickListener() {
-		public void onItemClick(AdapterView<?> parent, View v, int pos, long id) {
-			DayListItem day = array.get(pos);
-			Date todayDate = new Date();
-			int startHour = Integer.valueOf(day.getStart().substring(0, 2));
-			int startMin = Integer.valueOf(day.getStart().substring(3, 5));
-			Date theDate = new Date(curYear - 1900, curMonth, curDate, startHour, startMin);
-			Log.v("ThedateDate", theDate.toString());
-			Log.v("TodayDate", todayDate.toString());
-
-			if (theDate.compareTo(todayDate) > 0) { 
-				if (!day.getSeats().equals(getString(R.string.noSeats))) {
-					Intent dialog = new Intent(getApplicationContext(), AttendantsDialogActivity.class);
-					dialog.putExtra("ID", day.getId());
-					dialog.putExtra("DATE", curDate);
-					dialog.putExtra("MONTH", curMonth);
-					dialog.putExtra("YEAR", curYear);
-					dialog.putExtra("START", day.getStart());
-					dialog.putExtra("SEATS", Integer.parseInt(day.getSeats()));
-					dialog.putExtra("END", day.getEnd());
-					startActivity(dialog);
-				}
-			} else {
-				Toast.makeText(getApplicationContext(), R.string.backInTime, Toast.LENGTH_LONG).show();
-			}
-		}
-	};
-
-	public void printEvents(int extra) {
-		String[] events = getDateInfo(extra);
-		String temp;
-
-		array.clear();
-		if (!events[0].equals("NOCONNECTION")) {
-			if (!events[0].equals("NORESULT")) {
-				if (events.length >= 5) {
-					for (int i = 0; i < events.length; i = i + 5) {
-						if (Integer.parseInt(events[i + 3]) < 1) {
-							temp = getResources().getString(R.string.noSeats);
-						} else {
-							temp = events[i + 3];
-						}
-						array.add(new DayListItem(Integer.valueOf(events[i]), events[i + 2].substring(0, 5), temp,
-								events[i + 4].substring(0, 5)));
-					}
-
-					adapter.notifyDataSetChanged();
-
-					String[] mon = getResources().getStringArray(R.array.calMonthStringsSwe);
-					curYear = Integer.parseInt(events[1].substring(0, 4));
-					curMonth = Integer.parseInt(events[1].substring(5, 7)) - 1;
-					curDate = Integer.parseInt(events[1].substring(8, 10));
-					tvDate.setText(curDate + " " + mon[curMonth]);
-					tvYear.setText(String.valueOf(curYear));
-				} else {
-					Toast.makeText(getApplicationContext(), getResources().getString(R.string.noticeNoDayEvents),
-							Toast.LENGTH_LONG).show();
-					if (extra == 1) {
-						int date = new Date(curYear, curMonth + 1, 0).getDate();
-						++curDate;
-						if (curDate > date) {
-							++curMonth;
-							curDate = 1;
-							if (curMonth > 11) {
-								curMonth = 0;
-								++curYear;
-							}
-						}
-					} else {
-						--curDate;
-						if (curDate < 1) {
-							--curMonth;
-							if (curMonth < 0) {
-								curMonth = 11;
-								--curYear;
-							}
-							curDate = new Date(curYear, curMonth + 1, 0).getDate();
-						}
-					}
-				}
-			} else {
-				Toast.makeText(getApplicationContext(), R.string.noResultDatabase, Toast.LENGTH_LONG).show();
-			}
-		} else {
-			Toast.makeText(getApplicationContext(), R.string.noInternet, Toast.LENGTH_LONG).show();
-		}
 	}
 
 	public void topBackButton(View v) {
@@ -197,7 +138,11 @@ public class DayActivity extends Activity {
 			}
 			curDate = new Date(curYear, curMonth + 1, 0).getDate();
 		}
-		printEvents(1);
+		if (ep != null && ep.getStatus() != AsyncTask.Status.FINISHED) {
+			ep.cancel(true);
+		}
+		ep = new EventPrinter();
+		ep.execute(1);
 	}
 
 	public void topNextButton(View v) {
@@ -211,61 +156,17 @@ public class DayActivity extends Activity {
 				++curYear;
 			}
 		}
-		printEvents(2);
+		if (ep != null && ep.getStatus() != AsyncTask.Status.FINISHED) {
+			ep.cancel(true);
+		}
+		ep = new EventPrinter();
+		ep.execute(1);
 	}
 
 	public void bottomBackClick(View v) {
 		finish();
 	}
 
-	public String[] getDateInfo(int extra) {
-		String[] tmp = { "NOCONNECTION" };
-		if (isNetworkConnected()) {
-			String result = "";
-			InputStream is = null;
-			// http post
-			try {
-				HttpClient httpclient = new DefaultHttpClient();
-				HttpPost httppost = new HttpPost(getResources().getString(R.string.httpRequestUrl));
-
-				List<NameValuePair> pairs = new ArrayList<NameValuePair>();
-				pairs.add(new BasicNameValuePair("case", "getDayInfo"));
-				pairs.add(new BasicNameValuePair("extra", "" + extra));
-				pairs.add(new BasicNameValuePair("date", "" + (curDate)));
-				pairs.add(new BasicNameValuePair("month", "" + (curMonth + 1)));
-				pairs.add(new BasicNameValuePair("year", "" + curYear));
-				httppost.setEntity(new UrlEncodedFormEntity(pairs));
-
-				HttpResponse response = httpclient.execute(httppost);
-				HttpEntity entity = response.getEntity();
-				is = entity.getContent();
-			} catch (Exception e) {
-				Log.e("log_tag", "Error in http connection " + e.toString());
-			}
-			// convert response to string
-			try {
-				BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
-				StringBuilder sb = new StringBuilder();
-				String line = null;
-				while ((line = reader.readLine()) != null) {
-					sb.append(line + "\n");
-				}
-				is.close();
-
-				result = sb.toString();
-				result = result.replace("[", "");
-				result = result.replace("]", "");
-				result = result.replace("\"", "");
-				Log.v("r", "r: " + result);
-				tmp = result.split(",");
-			} catch (StringIndexOutOfBoundsException e) {
-				tmp[0] = "NORESULT";
-			} catch (Exception e) {
-				Log.e("log_tag", "Error converting result " + e.toString());
-			}
-		}
-		return tmp;
-	}
 
 	private boolean isNetworkConnected() {
 		getApplicationContext();
@@ -276,5 +177,126 @@ public class DayActivity extends Activity {
 			return false;
 		} else
 			return true;
+	}
+
+	private class EventPrinter extends AsyncTask<Integer, Void, String[]> {
+		private int extra;
+		
+		@Override
+		protected String[] doInBackground(Integer... extra) {
+			this.extra = extra[0];
+			String[] tmp = { "NOCONNECTION" };
+			if (isNetworkConnected()) {
+				String result = "";
+				InputStream is = null;
+				// http post
+				try {
+					HttpClient httpclient = new DefaultHttpClient();
+					HttpPost httppost = new HttpPost(getResources().getString(R.string.httpRequestUrl));
+
+					List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+					pairs.add(new BasicNameValuePair("case", "getDayInfo"));
+					pairs.add(new BasicNameValuePair("extra", "" + this.extra));
+					pairs.add(new BasicNameValuePair("date", "" + (curDate)));
+					pairs.add(new BasicNameValuePair("month", "" + (curMonth + 1)));
+					pairs.add(new BasicNameValuePair("year", "" + curYear));
+					httppost.setEntity(new UrlEncodedFormEntity(pairs));
+					
+					if (isCancelled())
+						return null;
+					HttpResponse response = httpclient.execute(httppost);
+					HttpEntity entity = response.getEntity();
+					is = entity.getContent();
+				} catch (Exception e) {
+					Log.e("log_tag", "Error in http connection " + e.toString());
+				}
+				// convert response to string
+				try {
+					BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
+					StringBuilder sb = new StringBuilder();
+					String line = null;
+					while ((line = reader.readLine()) != null) {
+						if (isCancelled())
+							return null;
+						sb.append(line + "\n");
+					}
+					is.close();
+
+					result = sb.toString();
+					result = result.replace("[", "");
+					result = result.replace("]", "");
+					result = result.replace("\"", "");
+					Log.v("r", "r: " + result);
+					tmp = result.split(",");
+				} catch (StringIndexOutOfBoundsException e) {
+					tmp[0] = "NORESULT";
+				} catch (Exception e) {
+					Log.e("log_tag", "Error converting result " + e.toString());
+				}
+			}
+			if (isCancelled())
+				return null;
+			return tmp;
+		}
+		
+		@Override
+		protected void onPostExecute(String[] events) {
+			String temp;
+
+			array.clear();
+			if (!events[0].equals("NOCONNECTION")) {
+				if (!events[0].equals("NORESULT")) {
+					if (events.length >= 5) {
+						for (int i = 0; i < events.length; i = i + 5) {
+							if (Integer.parseInt(events[i + 3]) < 1) {
+								temp = getResources().getString(R.string.noSeats);
+							} else {
+								temp = events[i + 3];
+							}
+							array.add(new DayListItem(Integer.valueOf(events[i]), events[i + 2].substring(0, 5), temp,
+									events[i + 4].substring(0, 5)));
+						}
+
+						adapter.notifyDataSetChanged();
+
+						String[] mon = getResources().getStringArray(R.array.calMonthStringsSwe);
+						curYear = Integer.parseInt(events[1].substring(0, 4));
+						curMonth = Integer.parseInt(events[1].substring(5, 7)) - 1;
+						curDate = Integer.parseInt(events[1].substring(8, 10));
+						tvDate.setText(curDate + " " + mon[curMonth]);
+						tvYear.setText(String.valueOf(curYear));
+					} else {
+						Toast.makeText(getApplicationContext(), getResources().getString(R.string.noticeNoDayEvents),
+								Toast.LENGTH_LONG).show();
+						if (extra == 1) {
+							int date = new Date(curYear, curMonth + 1, 0).getDate();
+							++curDate;
+							if (curDate > date) {
+								++curMonth;
+								curDate = 1;
+								if (curMonth > 11) {
+									curMonth = 0;
+									++curYear;
+								}
+							}
+						} else {
+							--curDate;
+							if (curDate < 1) {
+								--curMonth;
+								if (curMonth < 0) {
+									curMonth = 11;
+									--curYear;
+								}
+								curDate = new Date(curYear, curMonth + 1, 0).getDate();
+							}
+						}
+					}
+				} else {
+					Toast.makeText(getApplicationContext(), R.string.noResultDatabase, Toast.LENGTH_LONG).show();
+				}
+			} else {
+				Toast.makeText(getApplicationContext(), R.string.noInternet, Toast.LENGTH_LONG).show();
+			}
+		}
 	}
 }
