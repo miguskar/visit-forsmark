81cc10a426de5f371f2e1e35ecacedb07d09cde8
Johan Thorsberg <sjt_91@hotmail.com>
2012-12-05 19:28:47 +0000

    git-svn-id: http://192.168.1.79/svn/visit-forsmark@236 22f7ca1f-e406-4609-ac91-1203f0195763

--- visit-forsmark/src/se/forsmark/visit/AboutApplicationActivity.java
+++ visit-forsmark/src/se/forsmark/visit/AboutApplicationActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
-import android.widget.ScrollView;
 import android.widget.TabHost;
 import android.widget.TabHost.OnTabChangeListener;
 import android.widget.TabHost.TabSpec;
